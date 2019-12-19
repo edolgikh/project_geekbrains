c53bcdec59ff0edca52e70bc516c49dacb0a51b9
edolgikh <Midsummer2212>
2019-12-19 20:30:16 +0300

    1-4 задания из практического задания;

---
+++ static_src/reducers/profileReducer.js
@@ -0,0 +1,10 @@
+
+
+const initialStore = {
+    userData:
+         { name: "Гик", email: 'geek@geekbrains.ru', phone: '+7 999 999 99 99' },
+};
+
+export default function profileReducer(store = initialStore, action) {
+    return store;
+}
