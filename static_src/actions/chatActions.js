5c1cea500b2401130c6f0e60242b8802d9fe520a
edolgikh <Midsummer2212>
2020-01-14 22:05:31 +0300

    практическое задание

--- static_src/actions/chatActions.js
+++ static_src/actions/chatActions.js
@@ -1,6 +1,18 @@
 export const ADD_CHAT = '@@chat/ADD_CHAT';
+export const DELETE_CHAT = '@@chat/DELETE_CHAT';
+export const ACTIVE_CHAT = '@@chat/ACTIVE_CHAT';
 
 export const addChat = (title) => ({
-    type: ADD_CHAT,
-    title,
+   type: ADD_CHAT,
+   title,
+});
+
+export const deleteChat = (chatId) => ({
+   type: DELETE_CHAT,
+   chatId,
+});
+
+export const activeChat = (chatId) => ({
+   type: ACTIVE_CHAT,
+   chatId,
 });
--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,19 +1,23 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './containers/Router'
-import { BrowserRouter } from 'react-router-dom';
+import Router from './components/Router'
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/integration/react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import initStore from './utils/store';
+import initStore, { history } from './utils/store';
+
+const { store, persistor } = initStore();
 
 ReactDOM.render(
-    <Provider store={ initStore() }>
-        <BrowserRouter>
-            <MuiThemeProvider>
-                <Router />
-            </MuiThemeProvider>
-        </BrowserRouter>
+    <Provider store={ store }>
+        <PersistGate loading={ null } persistor={ persistor }>
+            <ConnectedRouter history={history}>
+                <MuiThemeProvider>
+                    <Router />
+                </MuiThemeProvider>
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>,
     document.getElementById('root'),
 );
